01934f9dba453ea76afce061e905a95a954c7bea
Prajwal Pai <prajpai@cisco.com>
2020-04-12 19:23:34 +0530

    First script

---
+++ guess-the-number.py
@@ -0,0 +1,18 @@
+from random import randint
+
+print("I am thinking of a number between 1 and 20.")
+actualnum = randint(0, 20)
+
+for attempt in range(1, 7):
+    num = int(input ("Guess the  number :"))
+    if num > actualnum:
+        print ("The number is smaller than your guess")
+    elif num < actualnum:
+        print("The number is Greater than your guess")
+    else:
+        break
+
+if (num == actualnum):
+    print ("Thats Impressive! Indeed the number I was thking is {}".format(num))
+else:
+    print("You exhausted your guesses! The number I was thinking was {}".format(actualnum))
